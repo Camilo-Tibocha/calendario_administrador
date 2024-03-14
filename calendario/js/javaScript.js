5dec4d2bfe0c09cc54b382203736222f753e5666
Sala G11 <CSalaG11@uniboyaca.edu.co>
2024-03-13 20:37:16 -0500

    modificacion

---
+++ calendario/js/javaScript.js
@@ -0,0 +1,47 @@
+document.getElementById("enero").addEventListener('click', ()=>{
+    
+});
+
+document.getElementById("febrero").addEventListener('click', ()=>{
+    
+});
+
+document.getElementById("marzo").addEventListener('click', ()=>{
+    
+});
+
+document.getElementById("abril").addEventListener('click', ()=>{
+    
+});
+
+document.getElementById("mayo").addEventListener('click', ()=>{
+    
+});
+
+document.getElementById("junio").addEventListener('click', ()=>{
+    
+});
+
+document.getElementById("julio").addEventListener('click', ()=>{
+    
+});
+
+document.getElementById("agosto").addEventListener('click', ()=>{
+    
+});
+
+document.getElementById("septiembre").addEventListener('click', ()=>{
+    
+});
+
+document.getElementById("octubre").addEventListener('click', ()=>{
+    
+});
+
+document.getElementById("noviembre").addEventListener('click', ()=>{
+    
+});
+
+document.getElementById("diciembre").addEventListener('click', ()=>{
+    
+});
