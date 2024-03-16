74ad0259298cce443fcba06bc0ecb7a81b66a514
Sala D9 <CSalaD9@uniboyaca.edu.co>
2024-03-16 12:04:52 -0500

    Modificacion calendario

--- calendario/js/javaScript.js
+++ calendario/js/javaScript.js
@@ -1,4 +1,6 @@
-function calcularDias(){
-    var meses = document.getElementById('mes');
-    
-}
+document.getElementById('mes').addEventListener('change',(event)=>{
+    console.log(event.target.value);
+    const mes = event.target.value;
+    const ul= (new Date(2024, parseInt(mes)+1 ,0)).getDate();
+    document.getElementById('dia').setAttribute('max', ul);
+})
