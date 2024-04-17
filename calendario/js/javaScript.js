d6511662c3de3277ada76715626be1604c728705
Camilo-Tibocha <actibocha@uniboyaca.edu.co>
2024-04-17 17:04:52 -0500

    Ultima modificacion

--- calendario/js/javaScript.js
+++ calendario/js/javaScript.js
@@ -4,3 +4,15 @@ document.getElementById('mes').addEventListener('change',(event)=>{
     const ul= (new Date(2024, parseInt(mes)+1 ,0)).getDate();
     document.getElementById('dia').setAttribute('max', ul);
 });
+
+
+
+document.getElementById('registrar').addEventListener('click', (event) => {
+if(document.getElementById('evento').value.trim() === ""){
+    alert('Ingrese el contenido del evento para poder registrarlo');
+    return;
+} else{
+    alert('El evento se ha registrado exitosamente.')
+    return;
+}
+});
